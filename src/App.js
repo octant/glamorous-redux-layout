18be2120ffdd4773d8dcdcaa8006e5c09499054c
Michael Wood <mwood@data-crush.com>
2018-03-06 23:31:27 -0500

    Removed references to layout in main app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 import { ThemeProvider } from 'glamorous'
-import { connect } from 'react-redux'
 
 import theme from './themes/main'
 import Content from './components/content-container'
@@ -26,50 +26,46 @@ class App extends Component {
     this.handleClick = this.handleClick.bind(this)
   }
 
-  handleClick (event) {
-    this.setState({
-      selectedLink: event.currentTarget.id
-    })
+  handleClick (type) {
+    return (event) => {
+      this.setState({
+        selectedLink: event.currentTarget.id
+      })
+    }
   }
 
   render () {
     return (
       <ThemeProvider theme={theme}>
         <div>
-          <Content layout={this.props.layout}>
+          <Content>
             <h1>Content</h1>
           </Content>
-          <SubNav layout={this.props.layout}>
+          <SubNav>
             <Links>
-              <Link id='3' onClick={this.handleClick} selected={this.state.selectedLink === '3'} position='top'>
+              <Link id='3' onClick={this.handleClick()} selected={this.state.selectedLink === '3'} position='top'>
                 About
               </Link>
-              <Link id='4' onClick={this.handleClick} selected={this.state.selectedLink === '4'} position='top'>
+              <Link id='4' onClick={this.handleClick()} selected={this.state.selectedLink === '4'} position='top'>
                 My Tasks
               </Link>
-              <Link id='5' onClick={this.handleClick} selected={this.state.selectedLink === '5'} position='top'>
+              <Link id='5' onClick={this.handleClick()} selected={this.state.selectedLink === '5'} position='top'>
                 Account
               </Link>
             </Links>
           </SubNav>
-          <SideBar layout={this.props.layout}>
+          <SideBar>
             <Links>
-              <Link id='0' onClick={this.handleClick} selected={this.state.selectedLink === '0'}><Icon className='fa fa-home' /> Home</Link>
-              <Link id='1' onClick={this.handleClick} selected={this.state.selectedLink === '1'}><Icon className='fa fa-tachometer' /> Dashboard</Link>
-              <Link id='2' onClick={this.handleClick} selected={this.state.selectedLink === '2'}><Icon className='fa fa-gears' /> Config</Link>
+              <Link id='0' onClick={this.handleClick()} selected={this.state.selectedLink === '0'}><Icon className='fa fa-home' /> Home</Link>
+              <Link id='1' onClick={this.handleClick()} selected={this.state.selectedLink === '1'}><Icon className='fa fa-tachometer' /> Dashboard</Link>
+              <Link id='2' onClick={this.handleClick()} selected={this.state.selectedLink === '2'}><Icon className='fa fa-gears' /> Config</Link>
             </Links>
           </SideBar>
-          <NavBar layout={this.props.layout} />
+          <NavBar />
         </div>
       </ThemeProvider>
     )
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    layout: state.layout
-  }
-}
-
-export default connect(mapStateToProps)(App)
+export default connect()(App)
